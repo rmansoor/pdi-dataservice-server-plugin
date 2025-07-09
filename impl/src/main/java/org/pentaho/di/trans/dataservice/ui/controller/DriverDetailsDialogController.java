d62a9a839b943dd70f3bb3fbbed10ccac0cd230e
Ramaiz Mansoor <ramaiz.mansoor@hitachivantara.com>
2025-07-09 09:40:20 -0400

    Update the location of the download for the driver bundle

--- impl/src/main/java/org/pentaho/di/trans/dataservice/ui/controller/DriverDetailsDialogController.java
+++ impl/src/main/java/org/pentaho/di/trans/dataservice/ui/controller/DriverDetailsDialogController.java
@@ -82,8 +82,7 @@ public class DriverDetailsDialogController extends AbstractController {
 
   File getDriverBundle() throws FileNotFoundException, AccessDeniedException {
     String driverFolderPath =
-        getString( PKG, "DriverDetailsDialog.DriverZipPath", PentahoSystem.getApplicationContext()
-            .getSolutionRootPath() );
+        getString( PKG, "DriverDetailsDialog.DriverZipPath", "." );
     File[] matchingFiles = new File( driverFolderPath ).listFiles( new FilenameFilter() {
       public boolean accept( File dir, String name ) {
         return name.startsWith( getString( PKG, "DriverDetailsDialog.DriverZipNamePrefix" ) ) && name.endsWith( "zip" );
